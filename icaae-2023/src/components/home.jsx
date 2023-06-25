11cd9eff82dd7d34f822f09349f6c9a0e0919b5e
nikhil <its.nikhil2808@gmail.com>
2023-06-25 11:54:40 +0530

    new file

---
+++ icaae-2023/src/components/home.jsx
@@ -0,0 +1,23 @@
+
+export default function Home(){
+    return(
+        <div className="navbar">
+        <img className="nav-logo" src=" " alt="logo"/>
+        <div className="nav">
+        
+           <a href="abc" className="xyz">HOME        </a>
+           <a href="abc" className="xyz">SPEAKERS    </a>
+           <a href="abc" className="xyz">COMMITTEE   </a>
+           <a href="abc" className="xyz">SPONSORSHIP </a>
+           <a href="abc" className="xyz">AWARDS      </a>
+           <a href="abc" className="xyz blue">REGISTRATION  </a>
+           <a href="abc" className="xyz">CONTACT US  </a>
+            
+        </div>
+
+            
+        </div>
+    )
+
+
+}
